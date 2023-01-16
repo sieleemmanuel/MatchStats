2c9e30bd020dcb713fee0b561a128a66a37feec9
kimsiele <sielekim.emmanuel@gmail.com>
2023-01-16 22:58:58 +0300

    Chore: Get fixtures functionality

--- app/src/main/java/com/siele/matchstats/data/api/MatchStatsApi.kt
+++ app/src/main/java/com/siele/matchstats/data/api/MatchStatsApi.kt
@@ -1,10 +1,21 @@
 package com.siele.matchstats.data.api
 
-import com.siele.matchstats.data.model.LeaguesResponse
+import com.siele.matchstats.data.model.fixtures.FixturesResponse
+import com.siele.matchstats.data.model.leagues.LeaguesResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
+import java.util.SimpleTimeZone
+import java.util.TimeZone
 
 interface MatchStatsApi {
     @GET("leagues")
     suspend fun getLeagues():Response<LeaguesResponse>
+
+    @GET("fixtures")
+    suspend fun getFixtures(
+        @Query("league") league:String ="",
+        @Query("season") season:String = "2022",
+        @Query("timezone") timeZone:String = TimeZone.getDefault().id
+    ):Response<FixturesResponse>
 }
