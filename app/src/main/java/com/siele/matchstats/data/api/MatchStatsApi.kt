dcc0aee6960fe4f2e2227ccb32f89c250afecef7
kimsiele <sielekim.emmanuel@gmail.com>
2023-01-18 23:59:36 +0300

    chore: get league standings and rounds

--- app/src/main/java/com/siele/matchstats/data/api/MatchStatsApi.kt
+++ app/src/main/java/com/siele/matchstats/data/api/MatchStatsApi.kt
@@ -1,11 +1,12 @@
 package com.siele.matchstats.data.api
 
 import com.siele.matchstats.data.model.fixtures.FixturesResponse
+import com.siele.matchstats.data.model.fixtures.RoundsResponse
 import com.siele.matchstats.data.model.leagues.LeaguesResponse
+import com.siele.matchstats.data.model.standings.StandingsResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
-import java.util.SimpleTimeZone
 import java.util.TimeZone
 
 interface MatchStatsApi {
@@ -18,4 +19,24 @@ interface MatchStatsApi {
         @Query("season") season:String = "2022",
         @Query("timezone") timeZone:String = TimeZone.getDefault().id
     ):Response<FixturesResponse>
+
+    @GET("fixtures/rounds")
+    suspend fun getRound(
+        @Query("league") league: String = "",
+        @Query("current") current: Boolean = true,
+        @Query("season") season: String = "2022",
+    ):Response<RoundsResponse>
+
+    @GET("fixtures/rounds")
+    suspend fun getLeagueRounds(
+        @Query("league") league: String = "",
+        @Query("season") season: String = "2022",
+    ):Response<RoundsResponse>
+
+
+    @GET("standing")
+    suspend fun getLeagueStanding(
+        @Query("league") league: String = "",
+        @Query("season") season: String = "2022",
+    ):Response<StandingsResponse>
 }
